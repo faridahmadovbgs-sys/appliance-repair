9b0127f76c1de3d4adca7d7a447f305a71a6b1fc
Work Order Admin <workorder@example.com>
2025-11-23 18:29:36 -0600

    Fix NextAuth dependencies and bcryptjs

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,6 +1,6 @@
 import NextAuth from "next-auth"
 import Credentials from "next-auth/providers/credentials"
-import bcrypt from "bcrypt"
+import bcrypt from "bcryptjs"
 import { getUserByEmail } from "./firestore"
 import { z } from "zod"
 
